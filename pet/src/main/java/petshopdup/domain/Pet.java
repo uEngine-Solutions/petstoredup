296923ac58439bf3a13c4f370a3956b6fe71b1dc
uEngine-Solutions <help@uengine.org>
2022-07-09 07:37:49 +0000

    tset

--- pet/src/main/java/petshopdup/domain/Pet.java
+++ pet/src/main/java/petshopdup/domain/Pet.java
@@ -24,11 +24,7 @@ public class Pet {
 
     private int energy;
     
-    private int newProperty;
-    private int prop2;
-    
-    private int prop3;
-    private int prop4;
+
 
     @Embedded
     private Address address;
@@ -77,9 +73,5 @@ public class Pet {
     public void feed(FeedCommand feedCommand) {}
     
     public void groom(){}
-    public void test(){}
-    public void test2(){}
 
-    public void test4(){}
-    public void test5(){}
 }
